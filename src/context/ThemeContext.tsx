1b9d68d9745ec7060342990619c2633b4ac1f7b1
Victor Arias <victorarias@Victor-Arias-MacBook-Pro.local>
2024-06-09 13:16:06 -0500

    Store theme in localStorage

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -2,13 +2,18 @@ import { createContext, useState } from "react";
 const ThemeContext = createContext(null);
 
 function ThemeContextProvider(props: any) {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState(
+    !localStorage.getItem("theme") ? "dark" : localStorage.getItem("theme")
+  );
 
   const toggleTheme = () => {
     setTheme((prevState) => {
       return prevState === "light" ? "dark" : "light";
     });
   };
+
+  localStorage.setItem("theme", theme);
+
   return (
     <ThemeContext.Provider value={{ theme: theme, toggleTheme: toggleTheme }}>
       {props.children}
